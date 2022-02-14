4aabc2ad7d01bb6c1e7663b037797ce4fabbb325
“changmeidong” <changmeidong_0123@163.com>
2022-02-14 21:47:53 +0800

    完成首页开发

--- assets/js/baseApi.js
+++ assets/js/baseApi.js
@@ -1,4 +1,10 @@
 $.ajaxPrefilter(function(options){
     // console.log(options.url);
    options.url  = "http://www.liulongbin.top:3008"+options.url
+    //添加同意的权限控制 
+   if(options.url.indexOf('/my/') != -1){
+        options.headers = {
+            Authorization: localStorage.getItem("token") || ''
+        }
+    }
 })
---
+++ assets/js/index.js
@@ -0,0 +1,56 @@
+$(function () {
+    getuserinfo();
+})
+
+var layer = layui.layer;
+//获取用户数据
+function getuserinfo() {
+    $.ajax({
+        type: "GET",
+        url: "/my/userinfo",
+        // headers: {
+        //     Authorization: localStorage.getItem("token") || ''
+        // },
+        success: function (res) {
+            if (res.code !== 0) {
+                return layer.msg(res.message);
+            }
+            renderAvatar(res.data)
+        },
+        complete: function (res) {
+            console.log(res);
+            if (res.responseJSON.code ===1
+                && res.responseJSON.message === "身份认证失败！") {
+                localStorage.removeItem('token');
+                location.href = '/login.html';
+            }
+        }
+
+    })
+}
+// 渲染用户的头像
+function renderAvatar(user) {
+    // 获取用户的名称
+    var name = user.nickname || user.username;
+    $('#welcome').html('欢迎&nbsp;&nbsp' + name);
+    // 渲染用户的头像
+    if (null != user.user_pic) {
+        $('.layui-nav-img').attr('src', user.user_pic).show();
+        $('.text-avatar').hide();
+    } else {
+        var first = name[0].toUpperCase();
+        $('.text-avatar').html(first).show();
+        $('.layui-nav-img').hide();
+    }
+}
+
+$('#btnLogout').on('click', function () {
+    layer.confirm('确定退出吗?',
+        function (index) {
+            //do something
+            console.log(index);
+            localStorage.removeItem('token');
+            location.href = '/login.html';
+            layer.close(index);
+        });
+})
